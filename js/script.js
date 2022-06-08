2780c43c867114c29b32b0c05640abbfe311f9c5
Den <example@gmail.com>
2022-06-08 19:10:36 +0300

    первое домашнее задание

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 // // работаем в современном режиме
-// "use strict";
+"use strict";
 
 
 // // let number = 5;
@@ -19,6 +19,31 @@
 
 // // полезные ссылки
 // // GitHowto
+
+// первое домашнее задание
+
+const numberOfFilm = +prompt("Сколько фильмов вы посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilm,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("Насколько оцениваете его?", ""),
+      d = prompt("Один из последних просмотренных фильмов?", ""),
+      c = prompt("Насколько оцениваете его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[d] = c;
+
+console.log(personalMovieDB);
+
+
+
 
 
 
