76296736486bc6dcf7f08e3dc116c7d9a27003ba
Den <example@gmail.com>
2022-06-08 13:00:57 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+// работаем в современном режиме
+"use strict";
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// // обьект
+
+// const obt = {
+//     name: 'Den',
+//     age: 23
+// };
+
+// console.log(obt.name);
+
+const result = confirm('Ты тут?');
+
+// полезные ссылки
+// GitHowto
+
+
+
+
+
+
+
+
